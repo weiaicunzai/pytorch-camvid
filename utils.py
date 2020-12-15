7886816e1ef8443e9c4673ffa9822103d72bcb9d
weiaicunzai <372995411@qq.com>
2020-12-15 23:06:03 +0800

    fix: missing imports Traceback (most recent call last):   File "train.py", line 97, in <module>     train_loader = utils.data_loader(args, 'train')   File "/data/by/pytorch-camvid/utils.py", line 299, in data_loader     transforms.RandomScaleCrop(settings.IMAGE_SIZE), NameError: name 'settings' is not defined

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ import cv2
 
 import transforms
 from dataset import CamVid, VOC2012Aug
+from conf import settings
 
 
 @torch.no_grad()
@@ -159,7 +160,7 @@ def get_model(model_name, input_channels, class_num):
         from models.segnet import SegNet
         net = SegNet(input_channels, class_num)
 
-    elif model_name == 'segnet':
+    elif model_name == 'deeplabv3plus':
         from models.deeplabv3plus import deeplabv3plus
         net = deeplabv3plus(class_num)
 
